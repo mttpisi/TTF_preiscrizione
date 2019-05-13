30d20468bbb8cfa0b513a8088f8a5e6dc49ce266
mttpisi <matteo.pisi@gmail.com>
2019-05-13 18:02:51 +0200

    Avanzato con il backend

--- backoffice/js/openday.js
+++ backoffice/js/openday.js
@@ -29,11 +29,11 @@ function generateOpenDayRow(){
 
 function createOpenDayRow(datiOpenDay,index){
     const openDayTableRow = `<div class="row tableRow" data-index="${index}">
-    <div class="col-md-2 py-3 px-0 col nomeField"><p class="mt-3">${datiOpenDay.titolo}</p></div>
-    <div class="col-md-2 py-3 px-0 col dataField"><p class="mt-3">${datiOpenDay.data.getDate() + "/" + (datiOpenDay.data.getMonth()+1) + "/" + datiOpenDay.data.getFullYear()}</p></div>
+    <div class="col-md-2 py-3 px-0 col"><p class="mt-3 nomeField">${datiOpenDay.titolo}</p></div>
+    <div class="col-md-2 py-3 px-0 col"><p class="mt-3 dataField">${datiOpenDay.data.getDate() + "/" + (datiOpenDay.data.getMonth()+1) + "/" + datiOpenDay.data.getFullYear()}</p></div>
     <div class="col-lg-8">
         <div class="row py-2">
-        <button class="btn my-1 btn-light col btnModifica" data-toggle="modal" data-target="#datiOpenDayModal">Modifica</button>
+        <button class="btn my-1 btn-light col btnModifica" onclick="modificaOpenDay(this)" data-toggle="modal" data-target="#datiOpenDayModal">Modifica</button>
         <button class="btn my-1 btn-light col btnEsportaLista">Esporta lista partecipanti</button>
         <button class="btn my-1 btn-light col btnInserisciPresenze" data-toggle="modal" data-target="#presenzeModal">Inserisci presenze</button>
         <button class="btn my-1 btn-danger col btnEliminaOpenDay" onclick="eliminaOpenDay(this)">Elimina</button>
@@ -48,4 +48,22 @@ function eliminaOpenDay(button){
     console.log(parentRow.dataset.index);
     //CONNETTERSI AL DATABASE E ELIMINARE LA ENTRY
     parentRow.parentNode.removeChild(parentRow);
+}
+
+function modificaOpenDay(button){
+    let parentRow = button.parentNode.parentNode.parentNode;
+    let indexOfInfos = parentRow.dataset.index;
+    let nomeField = "mockup";
+    let dataField = new Date();
+    console.log(nomeField);
+    document.getElementById("inputNomeOpenDay").value = nomeField;
+    document.getElementById("inputDataOpenDay").valueAsDate = dataField;
+}
+
+function aggiungiOpenDay(){
+
+}
+
+function resetModal(){
+    
 }
