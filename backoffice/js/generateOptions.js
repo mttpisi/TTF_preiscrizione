a66ff21579285a94a5a96f11c2a9ccbd5c6f3165
MttPisi <matteo.pisi@gmail.com>
2019-05-16 10:40:51 +0200

    Implementato Aggiungi,Modifica,Elimina

--- backoffice/js/generateOptions.js
+++ backoffice/js/generateOptions.js
@@ -1,6 +1,5 @@
 let ora = document.getElementById("oraOpenDay");
 let minuti = document.getElementById("minutiOpenDay");
-let option = document.createElement("option");
 for(let i=1;i<=24;i++){
     let option = document.createElement("option");
     option.value = i;
--- backoffice/js/openday.js
+++ backoffice/js/openday.js
@@ -10,60 +10,138 @@ function toggleChevron(node){
     node.childNodes[1].classList.toggle("fa-chevron-up");
 }
 
-function generateOpenDayRow(){
-    //CONNETTERSI AL DB
-
-    let datiOpenDay = {
-        titolo:"NomeOpenDay",
-        data: new Date,
-        luogo:"Milano",
-        id: 12345
+function btnToggle(flag){
+    if(flag){
+        document.getElementById("confermaAdd").classList.remove("d-none");
+        document.getElementById("confermaMod").classList.add("d-none");
+    }else{
+        document.getElementById("confermaAdd").classList.add("d-none");
+        document.getElementById("confermaMod").classList.remove("d-none");
     }
-    let opens = [datiOpenDay,datiOpenDay,datiOpenDay];
-    for(let i=0;i<opens.length;i++){
-        document.getElementById("tableOpenDay").innerHTML+=createOpenDayRow(datiOpenDay,i);
-    }
-    
-
 }
 
-function createOpenDayRow(datiOpenDay,index){
-    const openDayTableRow = `<div class="row tableRow" data-index="${index}">
+
+function resetModal(){
+    document.getElementById("formDatiOpenDay").reset();
+}
+
+
+let openDays;
+getOpenDays();
+function getOpenDays(){
+    fetch("http://localhost:3000/openDay/",{method:"GET"})
+    .then((response)=>{
+        return response.json();
+    }).then((data)=>{
+        openDays = data;
+        generateOpenDayRows();
+    });
+}
+
+function generateOpenDayRows(){
+    for(let i=0;i<openDays.length;i++){
+        document.getElementById("tableOpenDay").innerHTML+=createOpenDayRow(openDays[i]);
+    }
+}
+
+function createOpenDayRow(datiOpenDay){
+    const data = new Date(datiOpenDay.data);
+    const openDayTableRow = `<div class="row tableRow" data-index="${datiOpenDay.id}">
     <div class="col-md-2 py-3 px-0 col"><p class="mt-3 nomeField">${datiOpenDay.titolo}</p></div>
-    <div class="col-md-2 py-3 px-0 col"><p class="mt-3 dataField">${datiOpenDay.data.getDate() + "/" + (datiOpenDay.data.getMonth()+1) + "/" + datiOpenDay.data.getFullYear()}</p></div>
+    <div class="col-md-2 py-3 px-0 col"><p class="mt-3 dataField">${data.getDate() + "/" + (data.getMonth()+1) + "/" + data.getFullYear()}</p></div>
     <div class="col-lg-8">
         <div class="row py-2">
-        <button class="btn my-1 btn-light col btnModifica" onclick="modificaOpenDay(this)" data-toggle="modal" data-target="#datiOpenDayModal">Modifica</button>
+        <button class="btn my-1 btn-light col btnModifica" onclick="modOpenDay(${datiOpenDay.id})" data-toggle="modal" data-target="#datiOpenDayModal">Modifica</button>
         <button class="btn my-1 btn-light col btnEsportaLista">Esporta lista partecipanti</button>
         <button class="btn my-1 btn-light col btnInserisciPresenze" data-toggle="modal" data-target="#presenzeModal">Inserisci presenze</button>
-        <button class="btn my-1 btn-danger col btnEliminaOpenDay" onclick="eliminaOpenDay(this)">Elimina</button>
+        <button class="btn my-1 btn-danger col btnEliminaOpenDay" onclick="delOpenDay(${datiOpenDay.id},this)">Elimina</button>
         </div>
     </div>
     </div>`;
     return openDayTableRow;
 }
 
-function eliminaOpenDay(button){
-    let parentRow = button.parentNode.parentNode.parentNode;
-    console.log(parentRow.dataset.index);
-    //CONNETTERSI AL DATABASE E ELIMINARE LA ENTRY
-    parentRow.parentNode.removeChild(parentRow);
-}
 
-function modificaOpenDay(button){
-    let parentRow = button.parentNode.parentNode.parentNode;
-    let indexOfInfos = parentRow.dataset.index;
-    let nomeField = "mockup";
-    let dataField = new Date();
-    console.log(nomeField);
-    document.getElementById("inputNomeOpenDay").value = nomeField;
-    document.getElementById("inputDataOpenDay").valueAsDate = dataField;
-}
 
-function aggiungiOpenDay(){
+function addOpenDay(flag,indexOpenDay){
+    let titolo = document.getElementById("inputNomeOpenDay");
+    let dataOpen = document.getElementById("inputDataOpenDay");
+    let ora = document.getElementById("oraOpenDay").value;
+    let minuti = document.getElementById("minutiOpenDay").value;
+    let luogo = document.getElementById("inputLuogoOpenDay");
 
-}
+    inputErrorSignal(luogo,luogo.reportValidity());
 
-function resetModal(){
+    if(ora == "false"){ora = false}
+    if(minuti == "false"){minuti = false}
+    inputErrorSignal(document.getElementById("minutiOpenDay"),minuti);
+    inputErrorSignal(document.getElementById("oraOpenDay"),ora);
+
+    inputErrorSignal(dataOpen,dataOpen.reportValidity());
+    inputErrorSignal(titolo,titolo.reportValidity());
+
     
+    if(titolo.reportValidity() && dataOpen.reportValidity() && ora && minuti && luogo.reportValidity()){
+        if(flag){
+            let form = {
+                "titolo":titolo.value,
+                "data":new Date(dataOpen.value),
+                "ora":document.getElementById("oraOpenDay").value,
+                "minuti":document.getElementById("minutiOpenDay").value,
+                "luogo":luogo.value,
+                "partecipanti":[]
+            };
+            fetch("http://localhost:3000/openDay/",{method:"POST",headers:{"Accept":"application/json","Content-Type":"application/json"},body:JSON.stringify(form)})
+            .then((response)=>{
+                return response.json();
+            }).then((data)=>{
+                document.getElementById("annulla").click();
+                document.getElementById("tableOpenDay").innerHTML = "";
+                getOpenDays();
+            });  
+        }else{
+            let index = document.getElementById("confermaMod").dataset.index - 1;
+            openDays[index].titolo = titolo.value;
+            openDays[index].data = new Date(dataOpen.value);
+            openDays[index].ora = document.getElementById("oraOpenDay").value;
+            openDays[index].minuti = document.getElementById("minutiOpenDay").value;
+            openDays[index].luogo = luogo.value;
+            fetch("http://localhost:3000/openDay/"+openDays[index].id,{method:"PATCH",headers:{"Accept":"application/json","Content-Type":"application/json"},body:JSON.stringify(openDays[index])})
+            .then((response)=>{
+                return response.json();
+            }).then((data)=>{
+                document.getElementById("annulla").click();
+                document.getElementById("tableOpenDay").innerHTML = "";
+                getOpenDays();
+            });
+        }
+    }
 }
+
+function modOpenDay(indexOpenDay){
+    indexOpenDay -= 1;
+    let data = new Date(openDays[indexOpenDay].data);
+    let titolo = openDays[indexOpenDay].titolo;
+    let ora = openDays[indexOpenDay].ora;
+    let minuti = openDays[indexOpenDay].minuti;
+    let luogo = openDays[indexOpenDay].luogo;
+    document.getElementById("inputNomeOpenDay").value = titolo;
+    document.getElementById("inputDataOpenDay").valueAsDate = data;
+    document.getElementById("oraOpenDay").value = ora;
+    document.getElementById("minutiOpenDay").value = minuti;
+    document.getElementById("inputLuogoOpenDay").value = luogo;
+    document.getElementById("confermaMod").dataset.index = openDays[indexOpenDay].id;
+    btnToggle(false);
+}
+
+
+function delOpenDay(idOpenDay,button){
+    fetch("http://localhost:3000/openDay/"+idOpenDay,{method:"DELETE"})
+    .then((response)=>{
+        return response.json();
+    }).then((data)=>{
+        parentRow = button.parentNode.parentNode.parentNode;
+        parentRow.parentNode.removeChild(parentRow);
+    });
+}
+
